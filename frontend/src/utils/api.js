96f55bda192baece7c66b22edae04ab6983ceb61
Leonid Gladkikh <leogladkikh@yandex.ru>
2022-04-16 10:37:58 +0200

    errors

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,94 +1,93 @@
 class Api {
-    constructor({ baseUrl, headers }) {
+    constructor({ baseUrl }) {
         this._baseUrl = baseUrl;
-        this._headers = headers;
     }
 
-    getInitialCards(token) {
+    getInitialCards() {
         return fetch(`${this._baseUrl}/cards`, {
             headers: {
-                ...this._headers,
-                'Authorization': `Bearer ${token}`,
-            },
-        })
-            .then(this._checkStatus)
-    }
-
-    getUserInfo(token) {
-        return fetch(`${this._baseUrl}/users/me`, {
-            headers: {
-                ...this._headers,
-                'Authorization': `Bearer ${token}`,
-            },
-        })
-            .then(this._checkStatus)
-    }
-
-    changeUserInfo(data, token) {
-        return fetch(`${this._baseUrl}/users/me`, {
-            method: 'PATCH',
-            headers: {
-                ...this._headers,
-                'Authorization': `Bearer ${token}`,
-                body: JSON.stringify({ name: data.name, about: data.about })
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
             }
         })
             .then(this._checkStatus)
     }
 
-    changeUserAvatar(data, token) {
+    getUserInfo() {
+        return fetch(`${this._baseUrl}/users/me`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
+        })
+            .then(this._checkStatus)
+    }
+
+    changeUserInfo(data) {
+        return fetch(`${this._baseUrl}/users/me`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            },
+            body: JSON.stringify({ name: data.name, about: data.about })
+        })
+            .then(this._checkStatus)
+    }
+
+    changeUserAvatar(data) {
         return fetch(`${this._baseUrl}/users/me/avatar`, {
             method: 'PATCH',
             headers: {
-                ...this._headers,
-                'Authorization': `Bearer ${token}`,
-                body: JSON.stringify({ avatar: data.avatar })
-            }
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            },
+            body: JSON.stringify({ avatar: data.avatar })
         })
             .then(this._checkStatus)
     }
 
-    addCard(data, token) {
+    addCard(data) {
         return fetch(`${this._baseUrl}/cards`, {
             method: 'POST',
             headers: {
-                ...this._headers,
-                'Authorization': `Bearer ${token}`,
-                body: JSON.stringify({ name: data.title, link: data.link })
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            },
+            body: JSON.stringify({ name: data.title, link: data.link })
+        })
+            .then(this._checkStatus)
+    }
+
+    deleteCard(data) {
+        return fetch(`${this._baseUrl}/cards/${data._id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
             }
         })
             .then(this._checkStatus)
     }
 
-    deleteCard(data, token) {
-        return fetch(`${this._baseUrl}/cards/${data._id}`, {
-            method: 'DELETE',
-            headers: {
-                ...this._headers,
-                'Authorization': `Bearer ${token}`,
-            },
-        })
-            .then(this._checkStatus)
-    }
-
-    addLike(cardId, isLiked, token) {
+    addLike(cardId, isLiked) {
         return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
             method: `${isLiked ? 'DELETE' : 'PUT'}`,
             headers: {
-                ...this._headers,
-                'Authorization': `Bearer ${token}`,
-            },
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
         })
             .then(this._checkStatus)
     }
 
-    deleteLike(data, token) {
+    deleteLike(data) {
         return fetch(`${this._baseUrl}/cards/${data._id}/likes`, {
             method: 'DELETE',
             headers: {
-                ...this._headers,
-                'Authorization': `Bearer ${token}`,
-            },
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            }
         })
             .then(this._checkStatus)
     }
@@ -103,12 +102,7 @@ class Api {
 }
 
 const api = new Api({
-    // baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-32',
     baseUrl: 'https://api.leogladkikh.nomoredomains.work',
-    headers: {
-        // authorization: '834693d9-1821-4fba-aca9-8dc02ca9ce04',
-        'Content-Type': 'application/json',
-    }
 });
 
 export default api;
